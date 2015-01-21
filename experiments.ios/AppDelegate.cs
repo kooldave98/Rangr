75bd763d2a6bed3bcd3abbaf6c1d1c94c9df45cf
davidolubajo <>
2015-01-21 21:49:15 +0000

    Added back the Rangr.ios as a new universal project, and it all now  seems to be working

--- experiments.ios/AppDelegate.cs
+++ experiments.ios/AppDelegate.cs
@@ -1,81 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 
 using Foundation;
 using UIKit;
-using App.Common;
-
 using Google.Maps;
 
-namespace rangr.ios
+namespace experiments.ios
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the
-    // User Interface of the application, as well as listening (and optionally responding) to
-    // application events from iOS.
     [Register("AppDelegate")]
     public partial class AppDelegate : UIApplicationDelegate
     {
-        const string MapsApiKey = "AIzaSyACSPtVSdTYtRYQTjNh1Y6sUmNtVpshP4o";
-        public static AppDelegate Shared;
-
-        private UIWindow window;
-        private UINavigationController navigation = new UINavigationController();
+        UIWindow window;
 
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            Shared = this;
-            MapServices.ProvideAPIKey(MapsApiKey);
+            MapServices.ProvideAPIKey("AIzaSyACSPtVSdTYtRYQTjNh1Y6sUmNtVpshP4o");
 
-            UIApplication.SharedApplication.SetStatusBarStyle(UIStatusBarStyle.LightContent, false);
-
-            window = new UIWindow(UIScreen.MainScreen.Bounds);
-            UINavigationBar.Appearance.SetTitleTextAttributes(new UITextAttributes
-                {
-                    TextColor = UIColor.White
-                });
-
-            if (AppGlobal.Current.CurrentUserAndConnectionExists)
+            window = new UIWindow(UIScreen.MainScreen.Bounds)
             {
-                show_feed();
-            }
-            else
-            {
-                show_login();
-            }
-
-            navigation.NavigationBar.TintColor = UIColor.White;
-            navigation.NavigationBar.BarTintColor = Color.Blue;
-
-            window.RootViewController = navigation;
-            window.MakeKeyAndVisible();
-            return true;
-        }
-
-        public void show_login()
-        {
-            var login = new LoginViewController();
-            login.LoginSucceeded += () =>
-            {
-                show_feed();
+                RootViewController = new MapViewController()
             };
-
-
-            navigation.PushViewController(login, true);
-        }
-
-        public void show_feed()
-        {
-            var vc = new PostListViewController();
-            vc.PostItemSelected += show_detail;
-            navigation.PushViewController(vc, true);
-        }
-
-        public void show_detail(Post post_item)
-        {
-            var dc = new MapViewController();
-            //dc.SetDetailItem(post_item);
-            navigation.PushViewController(dc, true);
+            window.MakeKeyAndVisible();
+            
+            return true;
         }
     }
 }
---
+++ rangr.ios/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using Xamarin;
+
+namespace rangr.ios
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            Insights.Initialize("5f5d7dc30fc0c3f61718fa1ffacecf065b9deb26");
+
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
