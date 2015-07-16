41fe6f0808ac2c6ee290c16958a04f56ad95558b
davidolubajo <>
2015-07-16 20:16:00 +0100

    Changed the baseUrl

--- App.Common/Resources.cs
+++ App.Common/Resources.cs
@@ -8,11 +8,11 @@ namespace App.Common
 {
 	public static class Resources
 	{
-		//public const string baseUrl = "http://rangrapp.azurewebsites.net/api";//Azure
+		public const string baseUrl = "http://rangrapp.azurewebsites.net/api";//Azure
 
         //public const string baseUrl = "http://192.168.0.18:1898/api";//Home Wifi
 
-        public const string baseUrl = "http://10.208.1.109:1898/api";//Office wifi
+        //public const string baseUrl = "http://10.208.1.109:1898/api";//Office wifi
 
 
 
