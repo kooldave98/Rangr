e240e8736221a4c70731f7eb06354b1402866843
David <kooldave98@hotmail.com>
2015-08-25 00:09:34 +0100

    Added the activities based architecture to the branch
    The Android project in the main branch is all fragments driven. This
    branch is really to explore an alternative fully Activities driven
    architecture

---
+++ CSharpClients/App.Android/AboutAppActivity.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Preferences;
+using Android.Content.PM;
+using AndroidResource = Android.Resource;
+
+namespace App.Android
+{
+	[Activity (Label = "About",
+		ScreenOrientation = ScreenOrientation.Portrait,
+		ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+	public class AboutAppActivity : PreferenceActivity
+	{
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+			AddPreferencesFromResource (Resource.Xml.preferences_general);
+
+			ActionBar.SetDisplayHomeAsUpEnabled (true);
+			ActionBar.SetDisplayShowHomeEnabled (true);
+		}
+
+
+		public override bool OnOptionsItemSelected (IMenuItem item)
+		{
+			switch (item.ItemId) {
+			case AndroidResource.Id.Home:
+				Finish ();
+				break;
+			}
+			return base.OnOptionsItemSelected (item);
+		}
+
+	}
+}
+
